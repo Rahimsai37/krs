15bdc4b876ae24fd2c41b36785d769b56e89f4e4
Rahimsai37 <66055178+Rahimsai37@users.noreply.github.com>
2020-05-28 15:44:22 +0530

    area of circle.py
    printing the area of circle

---
+++ area.py
@@ -0,0 +1,3 @@
+r= float(input(" input the radius of circle: "))     #r=radius
+a=(22/7)*r*r                                         #a=area 
+print("the area of circle with radius %f is: %f" %(r,a))
